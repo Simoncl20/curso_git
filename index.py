0718d013e4fccf06075d98e5ccfae9c7af9f8058
simonc2 <simoncalderonl2010@gmail.com>
2021-09-29 13:16:51 -0500

    Se añadio otra función y se agrego un condicional

--- index.py
+++ index.py
@@ -5,3 +5,7 @@ variable = int(input("Introduzca un digito: "))
 segunda_variable = int(input("Introduzca otro digito: "))
 
 
+if variable > segunda_variable:
+
+    variable = segunda_variable
+
--- readme.py
+++ readme.py
@@ -2,3 +2,7 @@ def nueva_funcion(value: int)-> int:
 
     return value * 2
 
+def otro_funcion(value: int)-> int:
+
+    return value * 3
+
